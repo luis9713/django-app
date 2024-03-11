11fe9370e6000247daffc07a2b81242e45bf83df
Luis Angel <laportillo.6987@unicesmag.edu.co>
2024-03-11 11:12:05 -0500

    modificacion

--- academics/models.py
+++ academics/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class User(models.Model):
     firstname = models.CharField(max_length=20)
-    lastname= models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
     age = models.IntegerField()
+    created_at = models.DateTimeField(default= datetime.datetime.now())
+    updated_at = models.DateTimeField(default= datetime.datetime.now())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField(default=True)
+    
 
     
