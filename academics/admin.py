9751ee5553b9bfea985b14e1693418ec6e9e34a6
Luis Angel <laportillo.6987@unicesmag.edu.co>
2024-03-17 20:27:02 -0500

    creación de modelos y vistas de administrador

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import User, identification_type, country, department, cities, person, student
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(identification_type)
+admin.site.register(country)
+admin.site.register(department)
+admin.site.register(cities)
+admin.site.register(person)
+admin.site.register(student)
--- academics/models.py
+++ academics/models.py
@@ -3,20 +3,59 @@ import datetime
 
 # Create your models here.
 class User(models.Model):
+    id= models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     email = models.EmailField(max_length=50, unique=True, blank=False, null=False)
     password = models.CharField(max_length=50, blank=False, null=False)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+
+class identification_type(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    abrebiation = models.CharField(max_length=5, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class country(models.Model):   
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    abrebiation = models.CharField(max_length=5, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
     
+class department(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    abrebiation = models.CharField(max_length=5, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    id_country = models.ForeignKey(country, on_delete=models.CASCADE, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+class cities(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
+    abrebiation = models.CharField(max_length=5, blank=False, null=False)
+    description = models.CharField(max_length=100, blank=False, null=False)
+    id_department = models.ForeignKey(department, on_delete=models.CASCADE, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
 class person (models.Model):
-    id = models.AutoField(primary_key=True, unique=True, blank=False, null=True)
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    id_ident_type = models.CharField(max_length=5, default='CC', blank=False, null=False)
+    id_ident_type = models.ForeignKey(identification_type, on_delete=models.CASCADE, blank=False, null=False)
     id_ident_number = models.IntegerField( unique=True, blank=False, null=False)
-    id_ident_exp_city = models.IntegerField(blank=False, null=False)
+    id_ident_exp_city = models.ForeignKey(cities, on_delete=models.CASCADE, blank=False, null=False)
     address = models.CharField(max_length= 50, blank=False, null=False)
     phone = models.CharField(max_length=50, blank=False, null=False)
     id_user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False) 
@@ -24,5 +63,15 @@ class person (models.Model):
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=True)
+    
+class student(models.Model):
+    id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
+    code= models.CharField(max_length=50, blank=False, null=False)
+    id_person = models.ForeignKey(person, on_delete=models.CASCADE, blank=False, null=False)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
+    updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
 
     
