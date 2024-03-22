a5a3b3f7109990cea02be64e707df7242c077b7b
Luis Angel <laportillo.6987@unicesmag.edu.co>
2024-03-22 17:17:46 -0500

    changes 23-03-2024

--- academics/models.py
+++ academics/models.py
@@ -10,6 +10,9 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.id + " " + self.email
 
 class identification_type(models.Model):
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
@@ -19,6 +22,9 @@ class identification_type(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.id + " "+ self.name
 
 class country(models.Model):   
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
@@ -28,6 +34,9 @@ class country(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.id + " " + self.name
     
 class department(models.Model):
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
@@ -39,6 +48,9 @@ class department(models.Model):
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
     
+    def __str__(self):
+        return self.id + " " + self.name
+    
 class cities(models.Model):
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     name = models.CharField(max_length=50, blank=False, null=False)
@@ -48,11 +60,15 @@ class cities(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.id + " " + self.name
 
 class person (models.Model):
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    id_user= models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
     id_ident_type = models.ForeignKey(identification_type, on_delete=models.CASCADE, blank=False, null=False)
     id_ident_number = models.IntegerField( unique=True, blank=False, null=False)
     id_ident_exp_city = models.ForeignKey(cities, on_delete=models.CASCADE, blank=False, null=False)
@@ -64,6 +80,9 @@ class person (models.Model):
     deleted_at = models.DateTimeField(null=True, blank=True)
     status = models.BooleanField(default=True)
     
+    def __str__(self):
+        return self.id + " " + self.first_name + " " + self.last_name
+    
 class student(models.Model):
     id = models.AutoField(primary_key=True, unique=True, blank=False, null=False)
     code= models.CharField(max_length=50, blank=False, null=False)
@@ -72,6 +91,9 @@ class student(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null = False, blank = False)
     updated_at = models.DateTimeField(auto_now=True, null = False, blank = False)
     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.id + " " + self.code
     
 
     
--- app/settings.py
+++ app/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'academics.apps.AcademicsConfig',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = []
